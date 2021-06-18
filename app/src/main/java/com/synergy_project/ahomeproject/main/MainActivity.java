793dcd27776bd4af164d87edc7886cfae48a180e
ruvic <ruvicgreyes@yahoo.com>
2021-06-18 12:46:11 +0800

    Test Merge

--- app/src/main/java/com/synergy_project/ahomeproject/main/MainActivity.java
+++ app/src/main/java/com/synergy_project/ahomeproject/main/MainActivity.java
@@ -23,7 +23,7 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import org.jetbrains.annotations.NotNull;
-//qweiuqriuqyrquiyquiqyeqiwueyqweiuqrewwere234234
+//Commit And Push
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
 
